ae632af3da58ac1497d679a42a6017595f01fd00
NicolòRicciardo <richmnico@gmail.com>
2024-03-18 15:27:01 +0100

    Replaced javax dependencies with jakarta for tomcat version 10.

--- src/main/java/com/example/jdbcWebExample/servlets/UserServlet.java
+++ src/main/java/com/example/jdbcWebExample/servlets/UserServlet.java
@@ -2,12 +2,11 @@ package com.example.jdbcWebExample.servlets;
 
 import java.io.IOException;
 
-import javax.inject.Named;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.example.jdbcWebExample.dao.UserDao;
 
